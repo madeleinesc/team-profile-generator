855d6b15fbba12f9d52a34d66309b3aec4fa5795
Madeleinesc <madeleine.s.cosway@gmail.com>
2021-11-12 16:28:37 +1030

    added packages to index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// packages needed for application
+const inquirer = require('inquirer');
+const fs = require('fs'); // to read/write files
+const jest = require('jest');
+
+
+// for generate markdown html
+const generateHTML = require("./src/generateHTML.js"); 

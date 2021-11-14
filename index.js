9d47b03a70d34ec409aa2fe4b4a5fa18ea2e3309
Madeleinesc <madeleine.s.cosway@gmail.com>
2021-11-14 12:29:28 +1030

    added module exports to index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-// packages needed for application
+// packages needed 
 const inquirer = require('inquirer');
 const fs = require('fs'); // to read/write files
 const jest = require('jest');
 
 
-// for generate markdown html
-const generateHTML = require("./src/generateHTML.js"); 
+// module exports
+const generateHTML = require("./src/generateHTML.js"); // for generate markdown html -> to create webpage
+const Employee = require("./lib/Employee.js");
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+
+
+

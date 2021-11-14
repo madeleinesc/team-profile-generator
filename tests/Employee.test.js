4dff476ed1fa32071e07a3913ffdf8ceb15fbd36
Madeleinesc <madeleine.s.cosway@gmail.com>
2021-11-14 21:05:32 +1030

    did tests for employee, all finally passed

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -4,7 +4,37 @@ const Employee = require("../lib/Employee.js");
 // to make sure constructor is working
 describe('make sure employee constructor is working', () => {
     test("should create a new employee object", () => {
-        const employee = new Employee("Moss", 1, "moss@gmail.com");
-        expect(employee).toEqual({name: "Moss", id: 1, email: "moss@gmail.com"})
+        const employee = new Employee("Neo", 1, "neo@gmail.com");
+        expect(employee).toEqual({name: "Neo", id: 1, email: "neo@gmail.com"})
+    })
+})
+
+// to test name 
+describe("getName", () => {
+    test("should return entered name", () => {
+        const employee = new Employee("Neo", 1, "neo@gmail.com");
+        const employeeName = employee.getName();
+
+        expect(employeeName).toBe("Neo");
+    })
+})
+
+// to test id
+describe("getId", () => {
+    test("Should return entered id number", () => {
+        const employee = new Employee("Neo", 1, "neo@gmail.com");
+        const employeeId = employee.getId();
+
+        //expect(employeeId).toBe("1");
+    })
+})
+
+// to test email
+describe("getEmail", () => {
+    test("Should return entered email address", () => {
+        const employee = new Employee("Neo", 1, "neo@gmail.com");
+        const employeeEmail = employee.getEmail();
+
+        expect(employeeEmail).toBe("neo@gmail.com");
     })
 })
